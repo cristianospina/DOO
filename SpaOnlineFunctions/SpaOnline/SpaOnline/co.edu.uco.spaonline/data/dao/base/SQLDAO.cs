23cbb574676e1a57d0e783e10087ca91718c528d
Cristian David Ospina Ospina <100448542+cristianospina@users.noreply.github.com>
2024-03-10 18:33:49 -0500

    prueba

---
+++ SpaOnlineFunctions/SpaOnline/SpaOnline/co.edu.uco.spaonline/data/dao/base/SQLDAO.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaOnline.co.edu.uco.spaonline.data.dao
+{
+    public class SQLDAO
+    {
+        private readonly ConnectionInfo _conexion;
+
+        protected SQLDAO (ConnectionInfo conexion) {
+            SetConexion(conexion);
+        }
+
+        private void SetConexion(ConnectionInfo conexion) { 
+            //this._conexion = conexion;
+        }
+    }
+}
---
+++ SpaOnlineFunctions/SpaOnline/SpaOnline/co.edu.uco.spaonline/data/dao/daofactory/MySQLDAOFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaOnline.co.edu.uco.spaonline.data.dao.daofactory
+{
+    internal class MySQLDAOFactory
+    {
+        SqlConnection sqldb = new SqlConnection(@"Data Source=localhost;Initial Catalog=ERPDBII; Integrated Security=SSPI");
+
+
+        public MySQLDAOFactory() {
+            AbrirConexion();        
+        }
+
+        protected void AbrirConexion() {
+                sqldb.Open();      
+        }
+        public void CerrarConexion()
+        {
+            if (sqldb != null && sqldb.State == System.Data.ConnectionState.Open)
+            {
+                sqldb.Close();
+                Console.WriteLine("Conexión cerrada correctamente.");
+            }
+        }
+        
+    }
+}
