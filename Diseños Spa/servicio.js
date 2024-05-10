6f53cb9637340642b2461fa5f760d55267e4b585
jhonatanG12353 <109559610+jhonatanG12353@users.noreply.github.com>
2024-05-10 11:33:44 -0500

    Arreglo de Frontend y mejora de eficiencia en el codigo, creacion de diagrama de actividades

--- Diseños Spa/servicio.js
+++ Diseños Spa/servicio.js
@@ -1,113 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const serviciosContainer = document.getElementById("tablaBody1");
 
-document.addEventListener("DOMContentLoaded", function() {
-    const serviciosContainer = document.getElementById("tablaBody");
+  async function obtenerInformacionServicio() {
+    try {
+      const apiUrl = "http://localhost:7105/api/Servicio/1";
+      const response = await fetch(apiUrl);
+      const data = await response.json();
 
-    
-    function obtenerInformacionServicio() {
-        
-        const apiUrl = "http://localhost:7105/api/Servicio/1";
+      if (data && data.servicio) {
+        const servicio = data.servicio;
+        const servicioElement = document.createElement("tr");
+        servicioElement.classList.add("servicio");
 
-        
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                
-                if (data && data.servicio) {
-                    
-                    const servicio = data.servicio;
-                    
+        const contenidoHTML = `
+          <td>${servicio.nombre}</td>
+          <td>${servicio.descripcion}</td>
+          <td>${servicio.tipoServicio}</td>
+          <td>${servicio.valorServicio}</td>`;
 
-                    
-                    const servicioElement = document.createElement("tr");
-                    servicioElement.classList.add("servicio");
-
-                    const nombreServicioElement = document.createElement("td");
-                    nombreServicioElement.textContent = servicio.nombre;
-
-                    const descripcionServicioElement = document.createElement("td");
-                    descripcionServicioElement.textContent = servicio.descripcion;
-
-                    const tipoServicioServicioElement = document.createElement("td");
-                    tipoServicioServicioElement.textContent = servicio.tipoServicio;
-
-                    const valorServicioServicioElement = document.createElement("td");
-                    valorServicioServicioElement.textContent = servicio.valorServicio;
-
-                    servicioElement.appendChild(nombreServicioElement);
-                    servicioElement.appendChild(descripcionServicioElement);
-                    servicioElement.appendChild(tipoServicioServicioElement);
-                    servicioElement.appendChild(valorServicioServicioElement);
-
-                    serviciosContainer.appendChild(servicioElement);
-                } else {
-                    
-                    serviciosContainer.textContent = "No se encontró información del servicio.";
-                }
-            })
-            .catch(error => {
-                console.error("Error al obtener el servicio:", error);
-                serviciosContainer.textContent = "Ocurrió un error al obtener el servicio. Por favor, inténtalo de nuevo más tarde.";
-            });
+        servicioElement.innerHTML = contenidoHTML;
+        serviciosContainer.appendChild(servicioElement);
+      } else {
+        serviciosContainer.textContent =
+          "No se encontró información del servicio.";
+      }
+    } catch (error) {
+      console.error("Error al obtener el servicio:", error);
+      serviciosContainer.textContent =
+        "Ocurrió un error al obtener el servicio. Por favor, inténtalo de nuevo más tarde.";
     }
+  }
 
-
-    obtenerInformacionServicio();
+  obtenerInformacionServicio();
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const serviciosContainer = document.getElementById("tablaBody2");
-
-    
-    function obtenerInformacionServicio() {
-        
-        const apiUrl = "http://localhost:7105/api/Servicio/2";
-
-        
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                
-                if (data && data.servicio) {
-                    
-                    const servicio = data.servicio;
-                    
-
-                    
-                    const servicioElement = document.createElement("tr");
-                    servicioElement.classList.add("servicio");
-
-                    const nombreServicioElement = document.createElement("td");
-                    nombreServicioElement.textContent = servicio.nombre;
-
-                    const descripcionServicioElement = document.createElement("td");
-                    descripcionServicioElement.textContent = servicio.descripcion;
-
-                    const tipoServicioServicioElement = document.createElement("td");
-                    tipoServicioServicioElement.textContent = servicio.tipoServicio;
-
-                    const valorServicioServicioElement = document.createElement("td");
-                    valorServicioServicioElement.textContent = servicio.valorServicio;
-
-                    servicioElement.appendChild(nombreServicioElement);
-                    servicioElement.appendChild(descripcionServicioElement);
-                    servicioElement.appendChild(tipoServicioServicioElement);
-                    servicioElement.appendChild(valorServicioServicioElement);
-
-                    serviciosContainer.appendChild(servicioElement);
-                } else {
-                    
-                    serviciosContainer.textContent = "No se encontró información del servicio.";
-                }
-            })
-            .catch(error => {
-                console.error("Error al obtener el servicio:", error);
-                serviciosContainer.textContent = "Ocurrió un error al obtener el servicio. Por favor, inténtalo de nuevo más tarde.";
-            });
-    }
-
-
-    obtenerInformacionServicio();
-});
 // Este es para Crear un servicio o bueno... esa es la idea
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('#Crear form');
